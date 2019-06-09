d6b3984b4da4601bcf4a118179fa6b23db38d574
meelaiser <meelaiser@gmail.com>
2019-06-09 04:09:26 +0000

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 # git clone https://aur.archlinux.org/yay.git 
 # cd yay
 # makepkg -si
-#yay -S vi-vim-symlink xkbset xcalib powerpill
-#sudo pacman -S bc xorg-xrandr gvim conky lxappearance arc-gtk-theme feh xorg-xmodmap terminus firefox grml-zsh-config ranger py3status compton keynav
+#yay -S vi-vim-symlink xkbset xcalib powerpill 
+#sudo pacman -S network-manager-applet fcitx fcitx-libpinyin fcitx-configtool wqy-microhei goldendict notify-osd blueman copyq bc xorg-xrandr gvim conky lxappearance arc-gtk-theme feh xorg-xmodmap terminus firefox grml-zsh-config ranger py3status compton keynav pcmanfm
+sudo sysytemctl enable bluetooth bumblebeed NetworkManager getty@tty1
 mv .Xmodmap ~/.Xmodmap
 mv .config/ ~/.config/
 mv .fehbg ~/.fehbg
