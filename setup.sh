7491dc1e127dfa2cb4531d4072b4bfb40c2aa352
meelaiser <meelaiser@gmail.com>
2019-06-12 17:03:07 +0000

    reset Print

--- setup.sh
+++ setup.sh
@@ -4,12 +4,14 @@
 # cd yay
 # makepkg -si
 #yay -S vi-vim-symlink xkbset xcalib powerpill 
-#sudo pacman -S network-manager-applet fcitx fcitx-libpinyin fcitx-configtool wqy-microhei goldendict notify-osd blueman copyq bc xorg-xrandr gvim conky lxappearance arc-gtk-theme feh xorg-xmodmap terminus firefox grml-zsh-config ranger py3status compton keynav pcmanfm
-sudo sysytemctl enable bluetooth bumblebeed NetworkManager getty@tty1
-mv .Xmodmap ~/.Xmodmap
-mv .config/ ~/.config/
-mv .fehbg ~/.fehbg
-mv .zshrc ~/.zshrc
-mv .keynavrc ~/.keynavrc
-mv .vimrc ~/.vimrc
-mv .zprofile ~/.zprofile
+#sudo pacman -S network-manager-applet fcitx fcitx-libpinyin fcitx-configtool wqy-microhei goldendict notify-osd blueman copyq bc xorg-xrandr gvim conky lxappearance arc-gtk-theme feh xorg-xmodmap terminus firefox grml-zsh-config ranger py3status compton keynav pcmanfm libreoffice mlocate notify-osd
+#sudo sysytemctl enable bluetooth bumblebeed NetworkManager getty@tty1
+#cp .Xmodmap ~/.Xmodmap
+#diff -urNa .config/ ~/.config/
+#cp -rif .config/* ~/.config/
+#cp .fehbg ~/.fehbg
+#cp .zshrc ~/.zshrc
+#cp .keynavrc ~/.keynavrc
+#cp .vimrc ~/.vimrc
+#cp .zprofile ~/.zprofile
+cp -if .config/i3status/config ~/.config/i3status/config
